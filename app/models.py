0c94e3d54d168649a88520d5d25f025a53f0ab7b
Moosong - macbook <moosong@macbook.com>
2019-07-24 01:15:21 +0900

    modified

--- app/models.py
+++ app/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Delivery(models.Model):
-    name= models.TextField(primary_key = True)
+    name= models.TextField()
     housetype = models.TextField()
     kind_of_news = models.TextField()
-    images = models.TextField(null=True)
-    etc = models.TextField(null=True)
+    images = models.TextField(null=True,blank=True)
+    etc = models.TextField(null=True,blank=True)
     def __str__(self):
         return self.name
